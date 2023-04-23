7e2309b00d65cc902fd4bbb4f11de1f9d0b11c7c
balaji24k <balaji325s@gmail.com>
2023-04-23 17:46:48 +0530

    Subscribing and Dispatching Actions Task-13

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "redux";
+import { createStore } from "redux";
 
 const counterReducer = (state = {counter : 0}, action) => {
     if (action.type === 'increment') {
@@ -24,9 +24,21 @@ const counterReducer = (state = {counter : 0}, action) => {
             counter : state.counter-2
         };
     }
+
+    if (action.type === 'INCREMENTBY5') {
+        return {
+            counter : state.counter+5
+        };
+    }
+
+    if (action.type === 'DECREMENTBY5') {
+        return {
+            counter : state.counter-5
+        };
+    }
     return state;
 }
 
-const store = configureStore(counterReducer);
+const store = createStore(counterReducer);
 
 export default store;
